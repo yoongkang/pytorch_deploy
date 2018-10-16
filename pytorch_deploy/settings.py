3aeb5833bc9e40aee55df518bfbe6b58e621784c
Yoong Kang Lim <yoongkang.lim@gmail.com>
2018-10-16 22:09:39 +1100

    CORS

--- pytorch_deploy/settings.py
+++ pytorch_deploy/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'testapp.apps.TestappConfig',
+    'corsheaders',
 ]
 
 if DEBUG:
@@ -46,6 +47,7 @@ if DEBUG:
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -122,3 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+CORS_ORIGIN_WHITELIST = {
+    '.yoongkang.com',
+}
